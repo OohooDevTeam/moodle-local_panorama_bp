fa374b290c36c108f07de658035df6460ca92f7f
Andrew McCann <amccann@ualberta.ca>
2012-08-14 10:44:43 -0600

    Fixed the settings page to work with other Panorama plugins.

--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -22,12 +23,26 @@
  * @copyright  2012 oohoo IT Services (http://oohoo.biz)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die;
 
 if (has_capability('local/panorama_business_process:edit', $systemcontext)) {
-    $ADMIN->add('root', new admin_category('panorama', get_string('panorama',  'local_panorama_bp')));
-    $ADMIN->add('panorama', new admin_category('panorama_settings', get_string('panorama_settings',  'local_panorama_bp')));
-    $ADMIN->add('panorama_settings', new admin_externalpage('panoramabp_settings', get_string('panorama_bp', 'local_panorama_bp'), "$CFG->wwwroot/local/panorama_bp/admin/settings.php", 'local/panorama_business_process:edit'));
-    $ADMIN->add('panorama', new admin_externalpage('panoramabp', get_string('panorama_bp', 'local_panorama_bp'), "$CFG->wwwroot/local/panorama_bp/view.php", 'local/panorama_business_process:edit'));
+
+    if (!$ADMIN->locate('panorama')) {
+        $ADMIN->add('root',
+                new admin_category('panorama', get_string('panorama',
+                                'local_panorama_bp')));
+    }
+
+    if (!$ADMIN->locate('panorama_settings')) {
+        $ADMIN->add('panorama',
+                new admin_category('panorama_settings', get_string('panorama_settings',
+                                'local_panorama_bp')));
+    }
+
+    $ADMIN->add('panorama_settings',
+            new admin_externalpage('panoramabp_settings', get_string('panorama_bp',
+                            'local_panorama_bp'), "$CFG->wwwroot/local/panorama_bp/admin/settings.php", 'local/panorama_business_process:edit'));
+    $ADMIN->add('panorama',
+            new admin_externalpage('panoramabp', get_string('panorama_bp',
+                            'local_panorama_bp'), "$CFG->wwwroot/local/panorama_bp/view.php", 'local/panorama_business_process:edit'));
 }
